4eccd91645b223465a868d5fc540b52339a7b297
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-02-08 22:52:16 +0300

    Add data to customers table

--- task_6.sql
+++ task_6.sql
@@ -8,4 +8,4 @@ INSERT INTO customer(
 VALUES
     (2,'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
     (3,'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
-    (4,'Nehemial Kamolu', 'eobed@sandtech.com', '126 Happiness  Ave.');
+    (4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
